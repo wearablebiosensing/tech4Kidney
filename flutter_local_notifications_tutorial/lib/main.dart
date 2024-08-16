fa834a379be69e3fd4452d5778f775cd61f8c0c0
Ved <vedpatel765@gmail.com>
2024-08-16 16:09:26 -0400

    add welcome page and navigation

--- flutter_local_notifications_tutorial/lib/main.dart
+++ flutter_local_notifications_tutorial/lib/main.dart
@@ -25,6 +25,133 @@ void main() async {
   runApp(const MyApp());
 }
 
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      backgroundColor: const Color(0xFFF6ECED),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 100),
+            const Text(
+              'Welcome!',
+              style: TextStyle(fontSize: 35, color: Color.fromARGB(255, 255, 159, 152), fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            Image.asset(
+              'assets/kidney_image.png',
+              width: 0.7*screenWidth,
+              height: 0.7*screenWidth,
+            ),
+            const Text(
+              'KidneyCareWear',
+              style: TextStyle(fontSize: 35, color: Color.fromARGB(255, 255, 159, 152), fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 50),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SurveyPage()),
+                );
+              },
+                style: ElevatedButton.styleFrom(
+                minimumSize: const Size(150, 60), // Set the minimum width and height for the button
+              ),
+              child: const Text(
+                'Start',
+                style: TextStyle(fontSize: 24),
+                ),
+            ),
+            const Spacer(),
+            const Text(
+              'Thank you for participating!',
+              style: TextStyle(fontSize: 20, color: Color.fromARGB(255, 112, 112, 112), fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 5),
+            const Text(
+              'Link to consent form',
+              style: TextStyle(fontSize: 15, color: Colors.blue, decoration: TextDecoration.underline),
+            ),
+            const SizedBox(height: 25),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SurveyPage extends StatelessWidget {
+  const SurveyPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 180),
+            const Text(
+              'Please complete your survey!',
+              style: TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 50),
+            ElevatedButton(
+              onPressed: () => _launchUrl('https://forms.gle/CU7Pxo7rAwTndDC27'),
+              child: const SizedBox(
+                  width: 150,
+                  child: Center(
+                      child: Text('Survey Link'))),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => _launchUrl('https://forms.gle/CU7Pxo7rAwTndDC27'),
+              child: const SizedBox(
+                  width: 150,
+                  child: Center(
+                      child: Text('Sleep Survey Link'))),
+            ),
+            const Spacer(),
+            const Text(
+              'Thank you! 🙏',
+              style: TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 100),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          AwesomeNotifications().createNotification(
+            content: NotificationContent(
+                id: 1,
+                channelKey: "basic_channel",
+                title: "Welcome to Sleep Survey App",
+                body: "Please fill out the following survey"),
+          );
+        },
+        child: const Icon(
+          Icons.notification_add,
+        ),
+      ),
+    );
+  }
+
+  void _launchUrl(String urlString) async {
+    final Uri url = Uri.parse(urlString);
+    if (!await launchUrl(url)) {
+      throw 'Could not launch $url';
+    }
+  }
+}
+
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
@@ -51,66 +178,10 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       title: 'Tech4Kidney',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor:const Color.fromARGB(255, 255, 159, 152)),
         useMaterial3: true,
       ),
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(height: 180),
-              const Text(
-                'Please complete your survey!',
-                style: TextStyle(fontSize: 24),
-              ),
-              const SizedBox(height: 50),
-              ElevatedButton(
-                onPressed: () => _launchUrl('https://forms.gle/CU7Pxo7rAwTndDC27'),
-                child: const SizedBox(
-                  width: 150,
-                  child: Center(
-                    child: Text('Survey Link'))),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () => _launchUrl('https://forms.gle/CU7Pxo7rAwTndDC27'),
-                child: const SizedBox(
-                  width: 150,
-                  child: Center(
-                    child: Text('Sleep Survey Link'))),
-              ),
-              const Spacer(),
-              const Text(
-                'Thank you! 🙏',
-                style: TextStyle(fontSize: 24),
-              ),
-              const SizedBox(height: 100),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            AwesomeNotifications().createNotification(
-              content: NotificationContent(
-                  id: 1,
-                  channelKey: "basic_channel",
-                  title: "Welcome to Sleep Survey App",
-                  body: "Please fill out the following survey"),
-            );
-          },
-          child: const Icon(
-            Icons.notification_add,
-          ),
-        ),
-      ),
+      home: const WelcomePage(),
     );
-  }
-
-  void _launchUrl(String urlString) async {
-    final Uri url = Uri.parse(urlString);
-    if (!await launchUrl(url)) {
-      throw 'Could not launch $url';
-    }
   }
 }
